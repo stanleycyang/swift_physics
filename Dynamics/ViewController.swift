905dce5fb1d576cc317adad69cb56a2fa6c66c16
stanleycyang <stanley@stanleycyang.com>
2015-11-23 17:35:45 -0800

    Initial Commit

---
+++ Dynamics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dynamics
+//
+//  Created by Stanley Yang on 11/23/15.
+//  Copyright © 2015 Stanley Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
